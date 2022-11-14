9bc238a1e062ebfe7f5b6e5665e02ce9479bbe31
Christian Diemers <cdiemers@microsoft.com>
2022-11-14 09:56:45 +0100

    Add random appendix to job name
    This allows scheduling of multiple parallel jobs

--- src/config.py
+++ src/config.py
@@ -18,6 +18,7 @@
 # Update the Batch and Storage account credential strings below with the values
 # unique to your accounts. These are used when constructing connection strings
 # for the Batch and Storage client objects.
+import random
 
 _BATCH_ACCOUNT_NAME = ''
 _BATCH_ACCOUNT_KEY = ''
@@ -28,4 +29,4 @@ _POOL_ID = 'LinuxFfmpegPool'
 _DEDICATED_POOL_NODE_COUNT = 0
 _LOW_PRIORITY_POOL_NODE_COUNT = 5
 _POOL_VM_SIZE = 'STANDARD_A1_v2'
-_JOB_ID = 'LinuxFfmpegJob'
+_JOB_ID = 'linuxlfmpegjob' + str(random.randint(0, 10000))
